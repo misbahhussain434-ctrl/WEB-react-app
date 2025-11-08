ffdd2ab5d4d2b147dffabe45df23d4091dc11df7
Misbah Hussain <misbahhussain434@gmail.com>
2025-11-08 08:03:40 +0000

    Updated About.jsx component

--- src/my_component/pages/About.jsx
+++ src/my_component/pages/About.jsx
@@ -5,14 +5,18 @@ function About() {
     <section id="about" className="about">
       <h2>About Me</h2>
       <p>
-        I am a React.js developer who loves building responsive and user-friendly web apps.{" "}
-        <span className="highlight-skill">💻</span>
+        gitI am a React.js developer whose building responsive and user-friendly web apps.{" "}
+        <span className="highlight-skill"></span>
         <br />
+
+
         I focus on clean code, elegant design, and smooth user experience.{" "}
-        <span className="highlight-skill">✨</span>
+        <span className="highlight-skill"></span>
         <br />
+
+
         I also focus on HTML for structure formation of websites, CSS for styling, and JavaScript for interactive and responsive websites.{" "}
-        <span className="highlight-skill">⚡</span>
+        <span className="highlight-skill"></span>
       </p>
     </section>
   );
